9bf98a98fc02c20e769657b8127cda5c5efa5d09
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-04-18 01:17:07 +0100

    Updated configurations for jb_2.5-based source

--- p700.mk
+++ p700.mk
@@ -20,19 +20,20 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/melfas-ts.kl:system/usr/keylayout/qwerty.kl \
     $(LOCAL_PATH)/configs/melfas-ts.kl:system/usr/keylayout/melfas-ts.kl
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+
 # Permission files
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml
 
 # Wifi
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/librasdioif.ko:system/lib/modules/librasdioif.ko \
     $(LOCAL_PATH)/prebuilt/wlan.ko:system/lib/modules/wlan.ko
 
 # HW HALS
 PRODUCT_PACKAGES += \
-    gps.p700 \
-    power.p700
+    gps.p700
 
 PRODUCT_NAME := full_p700
 PRODUCT_DEVICE := p700
